fb2b1bed4aad9fcdf8b0b82a8e9af5ce107ecaa9
andydangit <andy.dang.it@gmail.com>
2024-10-10 12:26:19 -0500

    Add: total cost texbox and the calculation

--- EVChargeCal/ContentView.swift
+++ EVChargeCal/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var kWh = 0.00
     @State private var ECost = 0.11
+    @State private var totalCost: Double = 0.00
     
     
     let decimalFormatter: NumberFormatter = {
@@ -31,16 +32,33 @@ struct ContentView: View {
         NavigationStack{
             Form {
                 Section("Total Energy Used:") {
-                    TextField("kWh", value: $kWh, formatter: decimalFormatter)
+                    TextField("Total Enegery in kWh", value: $kWh, formatter: decimalFormatter)
                         .keyboardType(.decimalPad)
-                   
+                        .onChange(of: kWh) {
+                                 totalCost = calculateTotalCost()
+                             }
+                    
                 }
                 
                 Section("Electricity Cost:"){
                     TextField("Enter the electricity cost", value: $ECost, formatter: currencyFormatter)
+                        .onChange(of: ECost) {                                             totalCost = calculateTotalCost()
+                        }
+                }
+                
+                VStack {
+                    Text("Your total cost is: ")
+                    Text(totalCost, format: .currency(code: "USD"))
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding()
+                    
+                    
                 }
                 
                 .navigationTitle("Energy usage calculator ")
+                
             }
         }
         
@@ -51,6 +69,7 @@ struct ContentView: View {
     func calculateTotalCost() -> Double {
         return kWh * ECost
     }
+    
     
     //MARK: - END OF VIEW BODY
 }
