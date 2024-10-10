b06f660ce96618f8bc814a5020ce74bb293904bd
andydangit <andy.dang.it@gmail.com>
2024-10-10 12:00:10 -0500

    Add: UiDesign and number formatter as well as function

--- EVChargeCal/ContentView.swift
+++ EVChargeCal/ContentView.swift
@@ -8,15 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var kWh = 0.00
+    @State private var ECost = 0.11
+    
+    
+    let decimalFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter
+    }()
+    
+    let currencyFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "USD"
+        return formatter
+    }()
+    
+    
+    //MARK: - VIEW
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form {
+                Section("Total Energy Used:") {
+                    TextField("kWh", value: $kWh, formatter: decimalFormatter)
+                        .keyboardType(.decimalPad)
+                   
+                }
+                
+                Section("Electricity Cost:"){
+                    TextField("Enter the electricity cost", value: $ECost, formatter: currencyFormatter)
+                }
+                
+                .navigationTitle("Energy usage calculator ")
+            }
         }
-        .padding()
+        
     }
+    
+    //MARK: - Function
+    
+    func calculateTotalCost() -> Double {
+        return kWh * ECost
+    }
+    
+    //MARK: - END OF VIEW BODY
 }
 
 #Preview {
