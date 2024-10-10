2de9cd0f94807c063ee445f3661ed9f3b96c79a6
andydangit <andy.dang.it@gmail.com>
2024-10-10 11:04:22 -0500

    Initial Commit

---
+++ EVChargeCal/EVChargeCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EVChargeCalApp.swift
+//  EVChargeCal
+//
+//  Created by Andy Dang It on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EVChargeCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
